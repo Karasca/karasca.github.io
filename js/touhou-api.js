16ff6feea90a5e565e4b3308cf81cc14a05120e2
Karasca <roman_nl@live.nl>
2022-06-17 23:41:58 +0200

    add api link

--- js/touhou-api.js
+++ js/touhou-api.js
@@ -1,7 +1,7 @@
 function updateTable(){	
 	var htmlResult = `<tr><th id="th-reg">Username</th><th id="th-reg">Seed</th>`;
 	
-	$.getJSON('http://localhost:3001/registrations/', function(data) {
+	$.getJSON('https://karasca.com/registrations/', function(data) {
 	data.forEach(e => {
 		if(e != "undefined"){
 			htmlResult += `<tr id="tr-reg"><td>${e.username}</td><td>${e.seed}</td></tr>`
