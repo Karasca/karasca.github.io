fcca00da32e2f600fa1c24c1a56498d28741b341
Roman Petrosyan <roman_nl@live.nl>
2022-06-20 23:02:02 +0200

    helpful koishi

--- js/touhou-api.js
+++ js/touhou-api.js
@@ -1,37 +1,43 @@
-function updateTable(){	
-	const evtSource = new EventSource('https://karasca.com/registrations/')
-	
+function updateTable(){		
+	const remoteUrl = "https://karasca.com/registrations"
+	const localUrl = "http://localhost:3000/registrations"
+
+	const evtSource = new EventSource(remoteUrl);
 	
 	let jsonRes = []
 
-	evtSource.onmessage = (e) => {
+	evtSource.addEventListener("message", (e) => {
+		console.log(e.data)
 		jsonRes = JSON.parse(e.data)
-		var htmlResult = `<tr><th id="th-reg">Username</th><th id="th-reg">Seed</th>`;
-		jsonRes.forEach((item) => {
-		  	htmlResult += `<tr id="tr-reg"><td>${item.username}</td><td>${item.seed}</td></tr>`
-		})
-		$('#registrations').html(htmlResult);
-		jsonRes = []
-	}
+		if(Array.isArray(jsonRes)){
+			var htmlResult = `<tr><th id="th-reg">Username</th><th id="th-reg">Seed</th>`;
+			jsonRes.forEach((item) => {
+				  htmlResult += `<tr id="tr-reg"><td>${item.username}</td><td>${item.seed}</td></tr>`
+			})
+			$('#registrations').html(htmlResult);
+			jsonRes = []
+		}
+	})
 
 	evtSource.addEventListener("clear", (e) => {
 		console.log(e.data);
-
 		htmlResult = `<tr><th id="th-reg">Username</th><th id="th-reg">Seed</th>`;
 		$('#registrations').html(htmlResult);
 		jsonRes = []
 	})
 
 	evtSource.addEventListener("open-bingo", (e) => {
-		console.log(e.data);
-		console.log("Bingo Open!");
+		console.log(e.data)
+		$('#message-status').html(`Registrations are open, press the <img src="images/copy.png" height = "16"/> icon next to the seed to copy the chat command and register your seed.`);
 	})
 
 	evtSource.addEventListener("close-bingo", (e) => {
-		console.log(e.data);
-		console.log("Bingo Closed!");
-	})
+		console.log(e.data)
+		console.log("close event")
+		$('#message-status').html(`Registrations are closed!`);
 
+	})
+	
 	$(window).on('beforeunload', function(){
 		evtSource.close();
 	});
